7d50944fa571392e967e03bfc5ee2339a490afa6
tonynazarov <tonynazarov@gmail.com>
2024-08-04 13:47:30 +1200

    added resume

--- js/main.js
+++ js/main.js
@@ -108,4 +108,10 @@
         skillsStyles();
     });
 
+    // Mobile menu
+    $('.menu-toggle').on("click", function () {
+        $('.site-nav').addClass('animate');
+        $('.site-nav').toggleClass('mobile-menu-hide');
+    });
+
 })(jQuery);
